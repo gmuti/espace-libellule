8b0fbffeba0081396a314b804d6a361084c63732
gmuti <gmutikanga@cedreo.com>
2025-01-12 19:16:54 +0100

    dernière correction

--- js/main.js
+++ js/main.js
@@ -110,3 +110,14 @@
     
 })(jQuery);
 
+// Contact form
+function sendMail(){
+    let parms = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        subject: document.getElementById('subject').value,
+        message: document.getElementById('message').value
+    };
+    emailjs.send('service_vlyalkj', 'template_tq04wee', parms).then(alert('Email sent successfully!'));
+}
+
